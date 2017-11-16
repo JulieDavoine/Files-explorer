b1bb05b76f057d60f700fbb55df6986d696eeaed
EpicWAX <quentinhusler@gmail.com>
2017-11-16 13:11:37 +0000

    adding file_explorer.php

---
+++ file_explorer.php
@@ -0,0 +1,34 @@
+<?php
+	// function what_in_my_file($fichier)
+	// {
+	// 	strcmp(, )
+	// }
+
+	if (isset($_GET['dossier']))
+	    $adresse = $_GET['dossier'];   
+	else
+	    $adresse="./";
+	$n = -2; // var a delete dans le futur
+	echo "-".$adresse."</br></br>";
+	$lalala = getcwd();
+	echo $lalala."</br>";
+	// echo 'Dossssssssier : '.strstr($lalala, strrchr($lalala, "/"))."</br>"; //test a delete
+	// echo 'CWD without useless information : '.strstr($adresse, "")."</br>"; // same here
+	echo 'Current work directory : '.strrchr($adresse, "/");
+
+
+
+	$dossier = opendir($adresse); 
+	echo '<fieldset><legend>Liste des fichiers :</legend><br>'; 
+	while ($fichier = readdir($dossier))
+	{
+	    $n++; // mettre les image a la place de cette varaiable
+	    if (($fichier != ".") && ($fichier != "..")) 
+	        if (is_dir($adresse.'/'.$fichier))
+	            echo '<a href="file_explorer.php?dossier='.$adresse.'/'.$fichier.'">'.$n.' - '.$fichier.'</a><br>';
+	        else
+	            echo '<a href='.$adresse.'/'.$fichier.'>'.$n.' - '.$fichier.'</a><br>';
+	}
+	closedir($dossier);
+	echo '<br></fieldset>';
+?>
